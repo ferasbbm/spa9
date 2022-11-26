9ec7126601cc3a2a32f66f2b63e3785133c31bc0
Feras El-Sharif <49439225+ferasbbm@users.noreply.github.com>
2022-11-26 17:59:59 +0200

    [feat]: update-vue-api-to-Compositional

--- resources/js/componets/post/index.vue
+++ resources/js/componets/post/index.vue
@@ -24,16 +24,16 @@
             <tbody>
             <tr v-for="(post, index) in posts" class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                 <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                   {{post.id}}
+                    {{ post.id }}
                 </th>
                 <td class="py-4 px-6">
-                   {{post.title}}
+                    {{ post.title }}
                 </td>
                 <td class="py-4 px-6">
-                   {{post.description}}
+                    {{ post.description }}
                 </td>
                 <td class="py-4 px-6">
-                   {{post.created_at}}
+                    {{ post.created_at }}
                 </td>
                 <td class="py-4 px-6">
                     <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
@@ -46,22 +46,35 @@
 
 </template>
 <script>
+import usePosts from "../../composable/posts";
+import {onMounted} from "vue";
+
 export default {
-    data() {
-        return {
-            posts: []
-        }
-    },
-    methods: {
-        getPosts() {
-            axios.get('/api/posts')
-                .then(response => this.posts = response.data)
-                .catch(errors => console.log(errors)
-                );
-        }
-    },
-    mounted() {
-        this.getPosts();
-    },
+
+    /*API: Compositional*/
+    setup() {
+        const {posts, getPosts} = usePosts();
+        onMounted(getPosts);
+        return {posts};
+    }
+
+
+    /*API: Optional*/
+    // data() {
+    //     return {
+    //         posts: []
+    //     }
+    // },
+    // methods: {
+    //     getPosts() {
+    //         axios.get('/api/posts')
+    //             .then(response => this.posts = response.data)
+    //             .catch(errors => console.log(errors)
+    //             );
+    //     }
+    // },
+    // mounted() {
+    //     this.getPosts();
+    // },
 }
 </script>
